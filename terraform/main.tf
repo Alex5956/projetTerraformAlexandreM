0200f6d5fddb3a6b93e45366690ebdbd54f5695b
alexandre marillesse <alexandre.marillesse@gmail.com>
2023-01-11 06:56:02 -0800

    commit du 11/01

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-resource "ovh_cloud_project_network_private" "reseauPrive"{
-  name="resauPrive${var.nomInstance}"
+resource "ovh_cloud_project_network_private" "private_network"{
+  name="resauPrive${var.instanceName}"
   service_name= var.service_name
   regions= var.region
   vlan_id= var.vlanId
 }
-resource "ovh_cloud_project_network_private_subnet" "sousReseau_GRA"{
-  network_id= ovh_cloud_projet_private_subnet.reseauPrive.id
+resource "ovh_cloud_project_network_private_subnet" "subnetwork_GRA"{
+  network_id= ovh_cloud_project_network_private.private_network.id
   service_name= var.service_name
   network=var.vlan_dhcp_reseau
   region= element(var.region,0)
@@ -14,13 +14,13 @@ resource "ovh_cloud_project_network_private_subnet" "sousReseau_GRA"{
   provider=ovh
   no_gateway= true
 }
-resource "ovh_cloud_project_network_private_subnet" "sousReseau_SGB"{
-  network_id = ovh_cloud_project_private_subnet.reseauPrive.id
+resource "ovh_cloud_project_network_private_subnet" "subnetwork_SBG"{
+  network_id = ovh_cloud_project_network_private.private_network.id
   service_name = var.service_name
   region= element(var.region,1)
   network= var.vlan_dhcp_reseau
   start=var.vlan_dhcp_start
-  end=var.vlan_dhchp_finish 
+  end=var.vlan_dhcp_finish 
   provider=ovh.ovh
   no_gateway=true
 }
@@ -39,8 +39,8 @@ resource  "openstack_compute_instance_v2" "front" {
   name="front_${var.instanceName}"
   provider =openstack.ovh
   flavor_name= var.flavorName
-  image_name= var.image_name
-  region = element(var.region[0])
+  image_name= var.imageName
+  region = element(var.region,0)
   
   key_pair= openstack_compute_keypair_v2.test_keypair[0].name
   
@@ -48,13 +48,13 @@ resource  "openstack_compute_instance_v2" "front" {
     name= "Ext-Net"
   }
   network{
-    name= ovh_cloud_projet_network_private.private_network.name
+    name= ovh_cloud_project_network_private.private_network.name
     fixed_ip_v4 ="192.168.${var.vlanId}.254"
     }
-  depends_on = [ovh_cloud_project_network_private_subnet.sousReseau_GRA]
+  depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_GRA]
   }
   
-  resource "openstack_compute_instance_v2" "back_GRA"{
+  resource "openstack_compute_instance_v2" "backend_GRA"{
     count= var.backNumberOfInstances
     name= "backend_$(var.instanceName}_GRA_${count.index+2}"
     provider= openstack.ovh
@@ -66,26 +66,26 @@ resource  "openstack_compute_instance_v2" "front" {
       name= "Ext-Net"
       }
     network{
-      name=ovh_cloud_project_network_private.reseauprive.name
+      name=ovh_cloud_project_network_private.private_network.name
       fixed_ip_v4="192.168.${var.vlanId}.${count.index+1}"
       }
-     depends_on=[ovh_cloud_project_network_private_subnet.sousReseau_GRA]
+     depends_on=[ovh_cloud_project_network_private_subnet.subnetwork_GRA]
      }
-  resource "openstack_compute_instance_v2" "back_SGB"{
-    count= var.BackNumberOfInstances
+  resource "openstack_compute_instance_v2" "back_SBG"{
+    count= var.backNumberOfInstances
     name = "backend_${var.instanceName}_sbg_${count.index+1}" 
     provider= openstack.ovh
-    image_name= var.image_name
-    flavor_name=var.falvorName
+    image_name= var.imageName
+    flavor_name=var.flavorName
     region = element(var.region,1)
     
     key_pair= openstack_compute_keypair_v2.test_keypair[1].name
     
     network{
-      name = ovh_cloud_project_network_private.reseauPrive.name
+      name = ovh_cloud_project_network_private.private_network.name
       fixed_ip_v4="192.168.${var.vlanId}.${count.index+101}"
       }
-    depends_on = [ovh_cloud_project_network_private_subnet.sousReseau_SGB]
+    depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_SBG]
     }
     
     
@@ -94,8 +94,8 @@ resource  "openstack_compute_instance_v2" "front" {
       content= templatefile("templates/inventory.yml",
         {
           front= openstack_compute_instance_v2.front.access_ip_v4,
-          backends_SGB= [for i , j in openstack_compute_instance_v2.back_SGB: j.access_ipv4],
-          backend_GRA = [for i , j in openstack_compute_instance_v2.back_GRA: j.access_ip_v4],
+          backends_SBG= [for i , j in openstack_compute_instance_v2.backend_sbg: j.access_ipv4],
+          backend_GRA = [for i , j in openstack_compute_instance_v2.backend_gra: j.access_ip_v4],
         }
       )
       }
