ccf3027abbb560772f1c2539a0ffb2e728a10ba3
alexandre marillesse <alexandre.marillesse@gmail.com>
2023-01-11 07:31:31 -0800

    commit de 16:31

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "ovh_cloud_project_network_private" "private_network"{
   regions= var.region
   vlan_id= var.vlanId
 }
-resource "ovh_cloud_project_network_private_subnet" "subnetwork_GRA"{
+resource "ovh_cloud_project_network_private_subnet" "subnetwork_gra"{
   network_id= ovh_cloud_project_network_private.private_network.id
   service_name= var.service_name
   network=var.vlan_dhcp_reseau
@@ -14,7 +14,7 @@ resource "ovh_cloud_project_network_private_subnet" "subnetwork_GRA"{
   provider=ovh
   no_gateway= true
 }
-resource "ovh_cloud_project_network_private_subnet" "subnetwork_SBG"{
+resource "ovh_cloud_project_network_private_subnet" "subnetwork_sbg"{
   network_id = ovh_cloud_project_network_private.private_network.id
   service_name = var.service_name
   region= element(var.region,1)
@@ -30,7 +30,7 @@ resource "ovh_cloud_project_network_private_subnet" "subnetwork_SBG"{
 resource "openstack_compute_keypair_v2" "test_keypair"{
   count= length(var.region)
   provider=openstack.ovh
-  name="sshkey_${var.instanceName}_${count.index%2==0 ? "GRA" :"SGB" }"
+  name="sshkey_${var.instanceName}_${count.index%2==0 ? "gra" :"SGB" }"
   public_key= file("~/.ssh/id_rsa.pub")
   region= element(var.region, count.index)
 }
@@ -51,12 +51,12 @@ resource  "openstack_compute_instance_v2" "front" {
     name= ovh_cloud_project_network_private.private_network.name
     fixed_ip_v4 ="192.168.${var.vlanId}.254"
     }
-  depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_GRA]
+  depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_gra]
   }
   
-  resource "openstack_compute_instance_v2" "backend_GRA"{
+  resource "openstack_compute_instance_v2" "backend_gra"{
     count= var.backNumberOfInstances
-    name= "backend_$(var.instanceName}_GRA_${count.index+2}"
+    name= "backend_$(var.instanceName}_gra_${count.index+2}"
     provider= openstack.ovh
     flavor_name=var.flavorName
     image_name= var.imageName
@@ -69,9 +69,9 @@ resource  "openstack_compute_instance_v2" "front" {
       name=ovh_cloud_project_network_private.private_network.name
       fixed_ip_v4="192.168.${var.vlanId}.${count.index+1}"
       }
-     depends_on=[ovh_cloud_project_network_private_subnet.subnetwork_GRA]
+     depends_on=[ovh_cloud_project_network_private_subnet.subnetwork_gra]
      }
-  resource "openstack_compute_instance_v2" "back_SBG"{
+  resource "openstack_compute_instance_v2" "backend_sbg"{
     count= var.backNumberOfInstances
     name = "backend_${var.instanceName}_sbg_${count.index+1}" 
     provider= openstack.ovh
@@ -85,17 +85,17 @@ resource  "openstack_compute_instance_v2" "front" {
       name = ovh_cloud_project_network_private.private_network.name
       fixed_ip_v4="192.168.${var.vlanId}.${count.index+101}"
       }
-    depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_SBG]
+    depends_on = [ovh_cloud_project_network_private_subnet.subnetwork_sbg]
     }
     
     
     resource "local_file" "inventory"{
       filename= "../ansible/inventory.yml"
-      content= templatefile("templates/inventory.yml",
+      content= templatefile("templates/inventory.tmpl",
         {
           front= openstack_compute_instance_v2.front.access_ip_v4,
-          backends_SBG= [for i , j in openstack_compute_instance_v2.backend_sbg: j.access_ipv4],
-          backend_GRA = [for i , j in openstack_compute_instance_v2.backend_gra: j.access_ip_v4],
+          backends_gra = [for k ,p in openstack_compute_instance_v2.backend_gra: p.access_ip_v4],
+                    backends_sbg= [for k , p in openstack_compute_instance_v2.backend_sbg: p.access_ip_v4],
         }
       )
       }
